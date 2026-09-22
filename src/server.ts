a1f0f9c8c24349cd67fad2179670d869a98e9622
Felita Benoy <felita.benoy@paconsulting.com>
2026-09-22 15:27:51 +0100

    feat: move booking endpoints into router

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,20 @@
-import express, { Request, Response} from "express";
+import express, { Request, Response } from "express";
+import bookingRouter from "./routes/bookings";
 
 const app = express();
 const port = 5000;
 
-app.get("/", (req: Request, res: Response) => {
-	res.status(200).json({
-		status: "active",
-		message: "CoSpace API is running",
-	});
+app.use(express.json());
+app.use("/bookings", bookingRouter);
+
+app.get("/", (_req: Request, res: Response) => {
+    res.status(200).json({
+        status: "active",
+        message: "CoSpace API is running",
+    });
 });
 
 app.listen(port);
 
 export default app;
+
