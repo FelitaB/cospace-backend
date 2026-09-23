c0b1533a7d42d6dd63b86bcbdc3d27b1cd066db4
Felita Benoy <felita.benoy@paconsulting.com>
2026-09-23 13:42:34 +0100

    Extend Express Request type with user property

---
+++ src/types/express.d.ts
@@ -0,0 +1,14 @@
+import "express";
+
+declare global {
+    namespace Express {
+        interface AuthUser {
+            id: string;
+            role: string;
+        }
+
+        interface Request {
+            user?: AuthUser;
+        }
+    }
+}
